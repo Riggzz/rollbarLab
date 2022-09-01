f20f823ed8f313fd72945437e2e9146e73e6a630
= <=>
2022-09-01 15:32:41 -0600

    pokemon

--- server.js
+++ server.js
@@ -10,48 +10,53 @@ const{ ROLLBARTOKEN } = process.env
 app.use(express.json())
 app.use(cors())
 
-const students = ['Jimmy', 'Timothy', 'Jimothy']
+const pokemon = ['Rayqauza', 'Totodile', 'Charizard']
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'))
 })
 
-app.get('/api/students', (req, res) => {
-    res.status(200).send(students)
+app.get('/api/pokemon', (req, res) => {
+    res.status(200).send(pokemon)
 })
 
-app.post('/api/students', (req, res) => {
+app.post('/api/pokemon', (req, res) => {
    let {name} = req.body
    rollbar.log(name)
 
-   const index = students.findIndex(student => {
-       return student === name
+   const index = pokemon.findIndex(pokemon => {
+       return pokemon === name
    })
 
    try {
        if (index === -1 && name !== '') {
-           students.push(name)
-           res.status(200).send(students)
+           pokemon.push(name)
+           res.status(200).send(pokemon)
        } else if (name === ''){
            res.status(400).send('You must enter a name.')
+           rollbar.error('user needs to enter pokemon name')
        } else {
-           res.status(400).send('That student already exists.')
+           res.status(400).send('That pokemon already exists.')
+           rollbar.info('pokemon already exists')
        }
    } catch (err) {
        console.log(err)
+       rollbar.error('error 501')
    }
 })
 
-app.delete('/api/students/:index', (req, res) => {
+app.delete('/api/pokemon/:index', (req, res) => {
     const targetIndex = +req.params.index
     
-    students.splice(targetIndex, 1)
-    res.status(200).send(students)
+    pokemon.splice(targetIndex, 1)
+    res.status(200).send(pokemon)
+    rollbar.info('success')
 })
 
 const port = process.env.PORT || 5050
 
 app.listen(port, () => console.log(`Server listening on ${port}`))
+rollbar.warning('website is up')
 
 // include and initialize the rollbar library with your access token
 var Rollbar = require('rollbar')
@@ -63,7 +68,3 @@ var rollbar = new Rollbar({
 
 // record a generic message and send it to Rollbar
 rollbar.log('Hello world!')
-rollbar.error('error 402')
-rollbar.info('rollbar is not working for everyone')
-rollbar.warning('website has potential to crash')
-rollbar.critical('website crashed')
