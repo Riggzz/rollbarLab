fb59ec95c7dce3e53c39435b282dbaf23f2909c4
= <=>
2022-09-01 15:43:12 -0600

    update4

--- server.js
+++ server.js
@@ -34,11 +34,11 @@ app.post('/api/pokemon', (req, res) => {
            res.status(200).send(pokemon)
        } else if (name === ''){
            res.status(400).send('You must enter a name.')
-           rollbar.error('user needs to enter pokemon name')
        } else {
            res.status(400).send('That pokemon already exists.')
-           rollbar.info('pokemon already exists')
        }
+       rollbar.error('user needs to enter pokemon name')
+       rollbar.info('pokemon already exists')
    } catch (err) {
        console.log(err)
        rollbar.error('error 501')
@@ -59,12 +59,12 @@ app.listen(port, () => console.log(`Server listening on ${port}`))
 rollbar.warning('website is up')
 
 // include and initialize the rollbar library with your access token
-var Rollbar = require('rollbar')
+var Rollbar = require("rollbar");
 var rollbar = new Rollbar({
   accessToken: ROLLBARTOKEN,
   captureUncaught: true,
-  captureUnhandledRejections: true,
-})
+  captureUnhandledRejections: true
+});
 
 // record a generic message and send it to Rollbar
-rollbar.log('Hello world!')
+rollbar.log("Hello world!");
